748b2850b3ae988e73a604a361215138163c6dfa
Felipe Macedo <felipeaamacedo1@gmail.com>
2021-03-14 09:33:10 +0100

    rounded addition

--- 04_utilitarios/src/App.tsx
+++ 04_utilitarios/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Bordas from './components/Bordas'
+
 function App() {
   return (
     <div className="App">
-      <h1>Testando</h1>
+      <Bordas />
     </div>
   );
 }
---
+++ 04_utilitarios/src/components/Bordas.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './Bordas.css'
+
+export default function Bordas(){
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-2 border">
+
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2 border ">
+
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2 border border-0">
+
+                </div>
+                <div className="col-2 border border-top-0">
+                    
+                </div>
+                <div className="col-2 border border-right-0">
+                    
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2 border border-bottom-0">
+
+                </div>
+                <div className="col-2 border border-left-0">
+                    
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+            </div>
+         <div className="row">
+                <div className="col-2 border border-primary">
+
+                </div>
+                <div className="col-2 border border-secondary">
+                    
+                </div>
+                <div className="col-2 border border-top-0">
+                    
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-2 border border-right-0">
+
+                </div>
+                <div className="col-2 border border-warning">
+                    
+                </div>
+                <div className="col-2 border">
+                    
+                </div>
+            </div>
+            </div>
+    )
+
+}
